acb11bfabca764d8759c4b81bd5633652010ef8a
Abenezer Getnet <getnetabenezar@gmail.com>
2024-12-01 23:00:56 +0300

    db and route setup

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import api from "./routes/api.js"
+import connectDb from "./config/db.js";
+// import web from "./routes/web.js"
 dotenv.config()
+connectDb()
 
-
-const port = process.env.PORT || 4000
+const port = process.env.PORT 
 const app = express()
-app.get('/',(req,res)=>{
-    res.send('Hello world')
-})
+//  app.use(express.json())
+
+
+// app.use('/',web);
+app.use('/api',api)
+
 app.listen(port,()=>{
     console.log('listening '+ port)
 })
