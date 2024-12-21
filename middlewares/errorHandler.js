b13e1c87e1475bf73aeafb6ab4740a643ae0a13e
Abenezer Getnet <getnetabenezar@gmail.com>
2024-12-21 17:32:28 +0300

    catch error hadnler and error handler middleware added

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+export const errorHandler =(error,req,res,next)=>{
+    error.statusCode = error.statusCode || 500
+    error.status = error.status || 'error'
+    res.status(error.statusCode).json({
+        status:error.status,
+        message:error.message
+    })
+}
+export const catchErrorHandler = (func)=>{
+    return (req,res,next)=>{
+        func(req,res,next).catch(err=>next(err))
+    }
+}
