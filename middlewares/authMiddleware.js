5d3bce21ff3bf33ac230d51089f05ee76d3aa435
Abenezer Getnet <getnetabenezar@gmail.com>
2024-12-21 17:31:11 +0300

    verify token added or auth middleware added

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+import customError from '../utils/customError.js';
+export const verifyToken =async(req,res,next)=>{
+    const token = req.headers['authorization'].split(" ")[1]
+
+    if(!token){
+        const err = new customError('Access denied.UnAuthenticated request',400)
+        return next(err)
+    }
+    const verified = jwt.verify(token,process.env.JWT_SECRET)
+    req.userId = verified;
+    next()
+};
+
+// export default verifyToken
