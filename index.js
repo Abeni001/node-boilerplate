fc3f33f17114abcf0e97cac406c00d8db255c13f
Abenezer Getnet <getnetabenezar@gmail.com>
2024-12-21 17:27:34 +0300

    error handler middleware and request converter middleware added

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import api from "./routes/api.js"
 import connectDb from "./config/db.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 // import web from "./routes/web.js"
 dotenv.config()
 connectDb()
 
 const port = process.env.PORT 
 const app = express()
-//  app.use(express.json())
+app.use(express.json())    // to convert request to json
 
 
 // app.use('/',web);
 app.use('/api',api)
-
+app.use(errorHandler)
 app.listen(port,()=>{
     console.log('listening '+ port)
 })
