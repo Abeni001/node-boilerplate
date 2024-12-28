6936b0f3834447556a3e8f87c44a625e59eba039
Abenezer Getnet <getnetabenezar@gmail.com>
2024-12-28 18:25:54 +0300

    method and pre built-in event introduced

--- models/user.js
+++ models/user.js
@@ -1,13 +1,26 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
     {
         name:{
             type:String
         },
-        password:String,
-        confirm_password:String,
+        password:{
+            type:String,
+            required:true,
+            select:false        //hide the password when the use model is accessed
+        },
+        confirm_password:{
+            type:String,
+            required:true,
+            validate:{
+                validator:function(val){
+                   return this.password == val
+                },
+                message:'the password does not match'
+            }
+        },
         email:{
             type:String,
             required:true,
@@ -17,11 +30,28 @@ const userSchema = new Schema(
             default:null,
             type:Date
         },
+        password_changed_at:{
+            default:null,
+            type:Date
+        },
     },
     {
         timestamps: true
     }
 )
+userSchema.pre('save',function(next){
+    if(this.isModified('password')){
+        // this.password = bcrypt.hash(12,this.password)    // we can also hash the password here
+        this.confirm_password = undefined
+    }
+    next()
+})
+userSchema.methods.isPasswordChanged = async function(time){
+        if(this.password_changed_at){
+           return time <  (this.password_changed_at.getTime() / 1000)
+        }
+        return false
+}
 userSchema.index({ email: 1 }, { unique: true });
 const User  = mongoose.model('user',userSchema)
 
